0a27387286eb8699705f8210ceb71571d4b84741
thanhleha <thanhlehavn@gmail.com>
2015-05-04 18:54:32 +0200

    "Added code skeleton for Logistic Regression"

---
+++ src/model/logistic_regression.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+from util.activation_functions import Activation
+from model.classifier import Classifier
+
+
+class LogisticRegression(Classifier):
+    """
+    A digit-7 recognizer based on logistic regression algorithm
+
+    Parameters
+    ----------
+    train : list
+    valid : list
+    test : list
+    learningRate : float
+    epochs : positive int
+
+    Attributes
+    ----------
+    trainingSet : list
+    validationSet : list
+    testSet : list
+    weight : list
+    learningRate : float
+    epochs : positive int
+    """
+
+    def __init__(self, train, valid, test, learningRate=0.01, epochs=50):
+
+        self.learningRate = learningRate
+        self.epochs = epochs
+
+        self.trainingSet = train
+        self.validationSet = valid
+        self.testSet = test
+
+        # Initialize the weight vector with small values
+        self.weight = 0.01*np.random.randn(self.trainingSet.input.shape[1])
+
+    def train(self):
+        """Train the Logistic Regression"""
+        # TODO: Here you have to implement the Logistic Regression Training
+        # Algorithm
+        # TODO: use self.trainingSet
+        # TODO: use self.validationSet
+
+    def classify(self, testInstance):
+        """Classify a single instance.
+
+        Parameters
+        ----------
+        testInstance : list of floats
+
+        Returns
+        -------
+        bool :
+            True if the testInstance is recognized as a 7, False otherwise.
+        """
+        # TODO: Here you have to implement the Logistic Regression Algorithm
+        # to classify a single instance
+        pass
+
+    def evaluate(self, test=None):
+        """Evaluate a whole dataset.
+
+        Parameters
+        ----------
+        test : the dataset to be classified
+        if no test, the test set associated to the classifier will be used
+
+        Returns
+        -------
+        List:
+            List of classified decisions for the dataset's entries.
+        """
+        if test is None:
+            test = self.testSet.input
+        # Once you can classify an instance, just use map for all of the test
+        # set.
+        return list(map(self.classify, test))
+
+    def fire(self, input):
+        return Activation.sigmoid(np.dot(np.array(input), self.weight))
